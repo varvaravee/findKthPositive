3cbc8fdffb0b6f96ee3165cd5dedd1f003b3ad87
Varvara Vorobieva <varvaravee@gmail.com>
2024-04-09 08:23:59 -0700

    Create findKthPositive.cpp

---
+++ findKthPositive.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int current=1;
+        int index=0;
+        int answer=0;
+
+        while (k) {
+            if (index<arr.size()) {
+                if (arr[index] == current)
+                    current++;
+                else {
+                    k--;
+                    answer = current;
+                }
+
+            }
+            else {
+                k--;
+                answer=current;
+            }
+            current++;
+        }
+        return answer;
+    }
+};
